35b7447ca3875731e1652f2806b42f1c720d0735
KauanBestel <kauan.bestell@gmail.com>
2024-03-21 17:31:42 -0300

    page: custom navigation button

---
+++ lib/presentation/widgets/custom_navigation_button.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+
+class NavButton extends StatefulWidget {
+  const NavButton({super.key});
+
+override
+state <NavNutton> createstate() => _NavButton();
+}
