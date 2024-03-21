116810b06b67214ad11f7937e85f492e26e8c2f6
KauanBestel <kauan.bestell@gmail.com>
2024-03-21 14:58:40 -0300

    major product widget changes

--- lib/presentation/widgets/custom_product_selection.dart
+++ lib/presentation/widgets/custom_product_selection.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class CustomProduct extends StatefulWidget {
-  const CustomProduct({
-    super.key,
-    required this.onPress,
-    required this.label,
-  });
+  const CustomProduct({super.key, required this.onPress, required this.label});
 
   final VoidCallback onPress;
   final String label;
@@ -16,27 +12,61 @@ class CustomProduct extends StatefulWidget {
 }
 
 class _CustomProductState extends State<CustomProduct> {
+  bool isHovered = false;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 90,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black,
-          width: 1.5,
-        ),
-        color: Colors.green,
-        borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(50),
-            bottomLeft: Radius.circular(50),
-            topRight: Radius.circular(10),
-            bottomRight: Radius.circular(10)),
-      ),
+    return InkWell(
+      onHover: (value) {
+        setState(() {
+          isHovered = value;
+        });
+      },
       child: Container(
-        margin: const EdgeInsets.only(left: 50, right: 10),
-        color: Colors.white,
-        height: 60,
+        height: 90,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: 1.5,
+          ),
+          color: Colors.green,
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(50),
+              bottomLeft: Radius.circular(50),
+              topRight: Radius.circular(10),
+              bottomRight: Radius.circular(10)),
+        ),
+//--------------------------------CONTAINER 2------------------------------------
+        child: Container(
+          margin: const EdgeInsets.only(left: 0, right: 10),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50), bottomLeft: Radius.circular(50)),
+          ),
+          height: 60,
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(200),
+                child: Image.asset(
+                  'assets/images/imagemRota2.png',
+                  height: 200,
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    widget.label,
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
