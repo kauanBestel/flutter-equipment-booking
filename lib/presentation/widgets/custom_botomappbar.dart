4da5ceba77a50edc78d8a9ec685593b571e699d0
alex165432 <alexandre270903@gmail.com>
2024-03-22 14:47:04 -0300

    senior moment

--- lib/presentation/widgets/custom_botomappbar.dart
+++ lib/presentation/widgets/custom_botomappbar.dart
@@ -2,7 +2,7 @@ import 'package:equipment_boking/presentation/screens/pages/calendar_page.dart';
 import 'package:equipment_boking/presentation/screens/pages/products_page.dart';
 import 'package:flutter/material.dart';
 
-class CustomBottomAppBar extends StatelessWidget {
+class CustomBottomAppBar extends StatefulWidget {
   const CustomBottomAppBar({
     super.key,
     this.buttonColor = const Color(0xFF6DC0F7),
@@ -11,45 +11,42 @@ class CustomBottomAppBar extends StatelessWidget {
   final Color buttonColor;
 
   @override
+  State<CustomBottomAppBar> createState() => _CustomBottomAppBarState();
+}
+
+class _CustomBottomAppBarState extends State<CustomBottomAppBar> {
+  int i = 0;
+  List<IconData> iconList = [Icons.shopping_cart, Icons.calendar_month];
+  //substituir icone por um objeto {icon: Icons.X, route: XPage}
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       margin: const EdgeInsets.all(20.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20.0),
-        color: buttonColor,
+        color: widget.buttonColor,
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          IconButton(
-            icon: const Icon(
-              Icons.shopping_cart,
-              color: Colors.white,
+          for (var i = 0; i < iconList.length; i++)
+              IconButton(
+              icon: Icon(
+                iconList[i],
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ProductsPage(),
+                    //substituir por iconList.route
+                  ),
+                );
+              },
             ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ProductsPage(),
-                ),
-              );
-            },
-          ),
-          IconButton(
-            icon: const Icon(
-              Icons.calendar_month,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const CalendarPage(),
-                ),
-              );
-            },
-          ),
         ],
       ),
     );
