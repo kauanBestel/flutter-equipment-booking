96aaa5102d98016c5474caf6db64f998f4dd1cb3
KauanBestel <kauan.bestell@gmail.com>
2024-07-02 16:03:45 -0300

    a

--- lib/presentation/screens/pages/admin_page.dart
+++ lib/presentation/screens/pages/admin_page.dart
@@ -9,34 +9,39 @@ class AdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Padding(
-          padding: EdgeInsets.only(top: 80, bottom: 20, left: 20, right: 20),
-          child: Column(
-            children: [
-              TextField(
+        body: const Center(
+          child: Padding(
+            padding: EdgeInsets.only(top: 80, bottom: 20, left: 20, right: 20),
+            child: Column(
+              children: [
+                TextField(
                   decoration: InputDecoration(
                       prefixIcon: Icon(Icons.search),
                       focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
                           borderSide: BorderSide(color: Colors.black)),
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(color: Colors.black)),
-                      hintText: 'Search'))
-            ],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      hintText: 'Search'),
+                )
+              ],
+            ),
           ),
         ),
-      ),
-      floatingActionButton: NavButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const ProductsPage1()),
-          );
-        },
-      ),
-      bottomNavigationBar: const BaseBottomAppBar()
-    );
+        floatingActionButton: NavButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ProductsPage1()),
+            );
+          },
+        ),
+        bottomNavigationBar: const BaseBottomAppBar());
   }
 }
