e90cc24ebb11e9fa6ae4d9e1363a2e4d88e0f9e2
MurilonND <murilonog2013@gmail.com>
2024-07-05 14:40:15 -0300

    user and product models

--- lib/functions/product/product_model.dart
+++ lib/functions/product/product_model.dart
@@ -5,12 +5,66 @@ class ProductModel {
   final int type;
   final List<Rent?>? rents;
 
-  ProductModel(this.id, this.availability, this.imagePath, this.type, this.rents);
+  ProductModel({
+    required this.id,
+    required this.availability,
+    required this.imagePath,
+    required this.type,
+    this.rents,
+  });
+
+  static ProductModel fromJson(Map<String, dynamic> json) {
+    List<Rent?>? rentsList = [];
+    if (json['rents'] != null) {
+      var rentsJson = json['rents'] as List<dynamic>;
+      rentsList = rentsJson.map((rent) => Rent.fromJson(rent)).toList();
+    }
+
+    return ProductModel(
+      id: json['id'],
+      availability: json['availability'],
+      imagePath: json['imagePath'],
+      type: json['type'],
+      rents: rentsList,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    List<Map<String, dynamic>?>? rentsJsonList;
+    if (rents != null) {
+      rentsJsonList = rents!.map((rent) => rent?.toJson()).toList();
+    }
+
+    return {
+      'id': id,
+      'availability': availability,
+      'imagePath': imagePath,
+      'type': type,
+      'rents': rentsJsonList,
+    };
+  }
 }
 
 class Rent {
   final String userId;
   final DateTime date;
 
-  Rent(this.userId, this.date);
+  Rent({
+    required this.userId,
+    required this.date,
+  });
+
+  static Rent fromJson(Map<String, dynamic> json) {
+    return Rent(
+      userId: json['userId'],
+      date: DateTime.parse(json['date']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'userId': userId,
+      'date': date.toIso8601String(),
+    };
+  }
 }
--- lib/functions/user/user_model.dart
+++ lib/functions/user/user_model.dart
@@ -4,5 +4,28 @@ class UserModel {
   final String email;
   final String username;
 
-  UserModel(this.id, this.email, this.role, this.username);
+  UserModel({
+    required this.id,
+    required this.role,
+    required this.email,
+    required this.username,
+  });
+
+  static UserModel fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      role: json['role'],
+      email: json['email'],
+      username: json['username'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'role': role,
+      'email': email,
+      'username': username,
+    };
+  }
 }
